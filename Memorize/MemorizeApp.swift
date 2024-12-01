e1e29f9df1aa1ea2781689d05a750e155184021e
Boutheina <boutheinabouchahda@gmail.com>
2024-12-01 12:55:21 +0100

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Boutheina Bouchahda on 1/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
